add8ff613881e64af197c0806670bc86aacdd33b
klovett84 <84836870+klovett84@users.noreply.github.com>
2021-07-21 09:08:44 -0500

    Changed layout of project previews and added contact form

--- js/main.js
+++ js/main.js
@@ -15,3 +15,54 @@ $(function(){
         $(".sublist-content").slideUp(500);
     });
 });
+
+// JQuery lightbox for project screenshots
+$(function(){
+    $('.preview').click(function(e) {
+        e.preventDefault();
+        var img_src = $(this).attr("src");
+
+        if ($('#lightbox').length > 0) { // if #lightbox exists
+            //insert img tag with clicked link's href as src value
+            $('body').html('<img src="' + img_src + '" />');
+            
+            //show lightbox window
+            $('#lightbox').show();
+        }
+
+        else { // if #lightbox does not exist
+            //create HTML markup for lightbox window
+            var lightbox =
+            '<div id="lightbox">' +
+                '<div id="lightbox-content">' +
+                    '<img src="' + img_src + '" />' +
+                    '<p>Click to close</p>' +
+                '</div>' +
+            '</div>';
+
+            //insert lightbox HTML into page
+            $('body').append(lightbox);
+        }
+    });
+    // Click anywhere to close lightbox window
+    $('body').on('click', '#lightbox', function() {
+        $('#lightbox').remove();
+    });
+});
+
+// JQuery for contact form popup
+$(function(){
+    $("#contact-btn").click(function(){
+        if ($(':animated').length) {
+            return false;
+        }
+        $("#contact-popup").slideDown(500);
+    });
+
+    $("#close").click(function(){
+        if ($(':animated').length) {
+            return false;
+        }
+        $("#contact-popup").slideUp(500);
+    });
+});
